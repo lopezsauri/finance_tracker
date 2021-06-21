6b71b4ef0c1b180b052bbb7c23169e3786fd83d0
Samuel Lopez <lopezsauri.s@gmail.com>
2021-06-21 17:29:25 -0500

    Usin partial to show in the My Portfolio view - FT Learning

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,11 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol) #I can directrly call this method 
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol) #This is the return method**** It is implicit in Ruby
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      ##client.price(ticker_symbol) #This is the return method**** It is implicit in Ruby
+    rescue => exception
+      return nil
+    end
   end
 end
